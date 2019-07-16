331c896ab2c9ec25fdbed49ad4f23f706b6474a0
Daniel Loewenstein <loewenstein.daniel@gmail.com>
2019-07-16 18:08:31 +1000

    Add GUI
    Implemented simple tkinter based gui
    
    * filebrowser
    * anonymization
    * conversion
    * progressbar

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import tkinter.filedialog
 import os
-import shutil.rmtree as rmdir
+import shutil
 import csv
 import subprocess
 import xml.etree.ElementTree as ET
@@ -124,27 +125,36 @@ def anonymize_ecg(dir, file):
     ano_info = ecg.get_ano_info()
     return ano_info
 
-def convert_files(in_dir, out_dir, target_form, anonymize = False):
+def convert_files(in_dir, out_dir, target_form, anonymize, progbar):
     files = os.listdir(in_dir)
+    progbar["value"] = 0
+    prog_step = float(100.0/len(files))
     if anonymize:
         anonymization_key = []
         anonymization_key.append(list(["pat_id", "ano_id", "acq_datetime", "acq_datetime_ano"]))
+        prog_step = prog_step/3
 
         temp_path = create_tempdir(out_dir)
 
         for file in files:
             convert_ecg(in_dir, temp_path, file, ("MUSE-XML", ".xml"))
+            progbar["value"] += prog_step
+            progbar.update()
 
         temp_files = os.listdir(temp_path)
 
         for file in temp_files:
             ano_info = anonymize_ecg(temp_path, file)
             anonymization_key.append(ano_info)
+            progbar["value"] += prog_step
+            progbar.update()
 
         for file in temp_files:
             convert_ecg(temp_path, out_dir, file, target_form)
+            progbar["value"] += prog_step
+            progbar.update()
 
-        rmdir(temp_path)
+        shutil.rmtree(temp_path)
 
         with open(str(out_dir + "/" + "anonymization_key.csv"), "w") as outfile:
             writer = csv.writer(outfile, lineterminator = "\n")
@@ -152,13 +162,69 @@ def convert_files(in_dir, out_dir, target_form, anonymize = False):
     else:
         for file in files:
             convert_ecg(in_dir, out_dir, file, target_form)
+            progbar["value"] += prog_step
+            progbar.update()
 
-tk.Tk().withdraw()
+class App(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.in_dir  = tk.StringVar()
+        self.out_dir = tk.StringVar()
+        self.target_format = tk.StringVar()
+        self.target_format.set(out_opts[2])
+        self.anonymize = tk.BooleanVar()
+        self.create_widgets()
 
-in_dir = tk.filedialog.askdirectory(title="Choose input directory")
-print(in_dir)
-out_dir = tk.filedialog.askdirectory(title="Choose output directory")
-print(out_dir)
+
+    def create_widgets(self):
+        self.in_button = ttk.Button(self)
+        self.in_button["text"] = "Choose input"
+        self.in_button["command"] = lambda: self.askdir(self.in_dir)
+        self.in_button["width"] = 14
+        self.in_button.pack(fill="x")
+
+        self.out_button = ttk.Button(self)
+        self.out_button["text"] = "Choose output"
+        self.out_button["command"] = lambda: self.askdir(self.out_dir)
+        self.out_button["width"] = 14
+        self.out_button.pack(fill="x")
+
+        self.blank = ttk.Label(self)
+        self.blank.pack(fill="x")
+        self.opt_label = ttk.Label(self)
+        self.opt_label["text"] = "Target format:"
+        self.opt_label.pack(fill="x")
+        self.opt_list = ttk.OptionMenu(self, self.target_format, *out_opts)
+        self.opt_list.pack(fill="x")
+
+        self.conv_button = ttk.Button(self)
+        self.conv_button["text"] = "Convert"
+        self.conv_button["command"] = self.convert
+        self.conv_button.pack(fill="x")
+
+        self.ano_box = ttk.Checkbutton(self)
+        self.ano_box["text"] = "Anonymize"
+        self.ano_box["variable"] = self.anonymize
+        self.ano_box.pack(fill="x")
+
+    def askdir(self, var, title = "Choose directory"):
+        dir = tk.filedialog.askdirectory(title = title)
+        var.set(dir)
+        print(var.get())
+
+    def convert(self):
+        popup = tk.Tk()
+        progressbar = ttk.Progressbar(popup, length = 100, mode = "determinate")
+        progressbar.pack(fill = "x")
+        label = ttk.Label(popup, text = "Converting files ...")
+        label.pack(fill = "x")
+        targ_opt, targ_ext = self.target_format.get().split("'")[1::2]
+        convert_files(self.in_dir.get(), self.out_dir.get(), (targ_opt, targ_ext), self.anonymize.get(), progressbar)
+        popup.destroy()
+        
+
 
 out_opts = [("DICOM",    ".dcm"),
             ("ISHNE",    ".ecg"),
@@ -172,4 +238,8 @@ in_opts = [".dcm",
            ".scp",
            ".ecg"]
 
-convert_files(in_dir, out_dir, out_opts[0], anonymize = True)
+root = tk.Tk()
+style = ttk.Style()
+style.theme_use("clam")
+app  = App(master = root)
+app.mainloop()
